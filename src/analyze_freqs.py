cca8e01cb6f04789d4742828c67801aa0dd3045e
anthonysicilia <anthonysicilia@users.noreply.github.com>
2020-11-19 10:57:04 -0500

    Uncommented code (commented to save load time).

--- src/analyze_freqs.py
+++ src/analyze_freqs.py
@@ -14,31 +14,31 @@ import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
     
-    # # commented out to save time after running once and saving data 
-    # trainset = LocalizedCOCO('coco_localized/train', 
-    #     'coco_localized/vocab.pickle')
+    # commented out to save time after running once and saving data 
+    trainset = LocalizedCOCO('coco_localized/train', 
+        'coco_localized/vocab.pickle')
     
-    # vocab = {v : k for k,v in trainset.vocab.items()}
+    vocab = {v : k for k,v in trainset.vocab.items()}
 
-    # train_loader = torch.utils.data.DataLoader(trainset,
-    #     batch_size=32, 
-    #     shuffle=True, 
-    #     num_workers=6)
+    train_loader = torch.utils.data.DataLoader(trainset,
+        batch_size=32, 
+        shuffle=True, 
+        num_workers=6)
     
-    # words = dict()
+    words = dict()
     
-    # for imgs, caps, caplens, traces in tqdm(train_loader):
-    #     for arr in caps:
-    #         text = []
-    #         for word in [vocab[w.item()] for w in arr]:
-    #             if word not in {'<start>', '<end>', '<pad>'}:
-    #                 text.append(word)
-    #         for j, (word, pos) in enumerate(nltk.pos_tag(text)):
-    #             if pos == 'NN':
-    #                 if word not in words:
-    #                     words[word] = []
-    #                 words[word].append(j)
-    # pickle.dump(words, open('coco_localized/counts.pickle', 'wb'))
+    for imgs, caps, caplens, traces in tqdm(train_loader):
+        for arr in caps:
+            text = []
+            for word in [vocab[w.item()] for w in arr]:
+                if word not in {'<start>', '<end>', '<pad>'}:
+                    text.append(word)
+            for j, (word, pos) in enumerate(nltk.pos_tag(text)):
+                if pos == 'NN':
+                    if word not in words:
+                        words[word] = []
+                    words[word].append(j)
+    pickle.dump(words, open('coco_localized/counts.pickle', 'wb'))
     words = pickle.load(open('coco_localized/counts.pickle', 'rb'))
     for k,v in words.items():
         if len(v) > 50:
